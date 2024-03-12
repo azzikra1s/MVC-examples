7916c996ade50b95571a3018aba0b9b1d7212f66
Az Zikra Ismunandar <129355051+azzikra1s@users.noreply.github.com>
2024-03-12 19:03:06 +0700

    Add files via upload

---
+++ app/model/user_model.php
@@ -0,0 +1,8 @@
+<?php
+    class user_model {
+        private $nama = "Az Zikra Ismunandar";
+        public function getUser() {
+            return $this->nama;
+        }
+    }
+?>
---
+++ app/view/user/profile.php
@@ -0,0 +1,5 @@
+<div class="container text-center mt-4">
+    <h1>Halaman User</h1>
+    <img src="<?= BASE_URL; ?>/img/pp.png" class="rounded-circle shadow" width="130" height="130" alt="profile">
+    <p>Halo, saya <?= $data['nama']; ?> seorang <?= $data['pekerjaan'];?>.</p>
+</div>
